8d6312b2528be45cf8aa53784c7b77fde64b1665
koodikimmo <7830545+koodikimmo@users.noreply.github.com>
2020-03-09 20:15:12 +0200

    Support for owner names/colors

--- app/reports.js
+++ app/reports.js
@@ -1,8 +1,12 @@
+const config = require("config");
+
 const discord = require("./discord")
+
+const DEFAULT_COLOR = "#aabbcc";
 
 function sendStoryCreatedMsg(story, owner_ids) {
 	const msg = discord.getMessageBuilder()
-        .setColor("#aabbcc")
+        .setColor(_getFirstOwnerColor(owner_ids))
         .setText("Story created")
         .addField("Name", story.name)
         .addField("Owner", _getPlainTextOwnerIds(owner_ids))
@@ -14,7 +18,7 @@ function sendStoryCreatedMsg(story, owner_ids) {
 
 function sendStoryAssignedMsg(story, owner_ids) {
 	const msg = discord.getMessageBuilder()
-        .setColor("#aabbcc")
+        .setColor(_getFirstOwnerColor(owner_ids))
         .setText("Story assigned")
         .addField("Name", story.name)
         .addField("Owner", _getPlainTextOwnerIds(owner_ids))
@@ -38,12 +42,43 @@ function _getPlainTextOwnerIds(owner_ids) {
 		}
 
 		// Add name
-		nameStr += owner_ids[i];
+		nameStr += _findPlainTextName(owner_ids[i]);;
 	}
 
 	return nameStr;
 }
 
+function _findPlainTextName(owner_id) {
+	let nameToUse = owner_id;
+
+	const configNameLocator = "clubhouse-users." + owner_id + ".name";
+	if(config.has(configNameLocator)) {
+		// Use a better name if we have one
+		nameToUse = config.get(configNameLocator);
+	}
+
+	return nameToUse;
+}
+
+function _getFirstOwnerColor(owner_ids) {
+	if(owner_ids == null || owner_ids.length === 0) {
+		return DEFAULT_COLOR;
+	}
+
+	return _findUserColor(owner_ids[0]);
+}
+
+function _findUserColor(owner_id) {
+	let colorToUse = DEFAULT_COLOR;
+
+	const configColorLocator = "clubhouse-users." + owner_id + ".color";
+	if(config.has(configColorLocator)) {
+		// Use a better color if we have one
+		colorToUse = config.get(configColorLocator);
+	}
+
+	return colorToUse;
+}
 
 module.exports = {
     sendStoryCreatedMsg,
